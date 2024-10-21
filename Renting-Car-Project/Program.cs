792769e31bc59ad50a61a6575a9d906b4bb06581
Ahamd-Fallah <152901603+Ahamd-Fallah@users.noreply.github.com>
2024-10-21 22:26:36 +0330

    adding System.Data.SqlClient lib

--- Renting-Car-Project/Program.cs
+++ Renting-Car-Project/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace Renting_Car_Project
 {
