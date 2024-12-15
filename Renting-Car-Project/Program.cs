40102ec109d0fb7ddf67c6759f68ce4a6bf96c49
Ahamd-Fallah <152901603+Ahamd-Fallah@users.noreply.github.com>
2024-12-15 17:52:46 +0330

    replacig UserSession instead of TokenManager

--- Renting-Car-Project/Program.cs
+++ Renting-Car-Project/Program.cs
@@ -20,12 +20,19 @@ namespace Renting_Car_Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            string token = TokenManager.LoadToken();
-            if (!string.IsNullOrEmpty(token)) {
+
+            // بارگذاری اطلاعات کاربر از فایل
+            var userSession = UserSession.LoadUserSession();
+
+            // بررسی اینکه آیا کاربر وارد شده است
+            if (userSession != null && userSession.IsLoggedIn)
+            {
+                // اگر کاربر وارد شده باشد، صفحه راه‌اندازی نمایش داده می‌شود
                 Application.Run(new StartupForm()); 
             }
             else
             {
+                // اگر کاربر وارد نشده باشد، صفحه راه‌اندازی نمایش داده می‌شود
                 Application.Run(new StartupForm()); 
             }
 
